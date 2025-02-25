adac405ecef6c26557e4217444bc6320322d1a08
Sukhada Kulkarni <sukhada91@gmail.com>
2025-02-25 09:19:56 -0800

    Fix link to old directory

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   trailingSlash: false,
-  baseUrl: '/withtheranks-astro/docs/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
